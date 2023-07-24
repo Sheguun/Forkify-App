de417aaa996d22134ba64e31cc2958086a86cf6a
sheguun <oluwasegunmichael8@gmail.com>
2023-07-24 15:23:46 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '50f59ef5-6824-4c29-a146-fb9c4fd1ae6a';
+export const MODAL_CLOSE_SEC = 2.5;
